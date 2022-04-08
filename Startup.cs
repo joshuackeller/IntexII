6ec226ee9f79e817a718baac6924061d12f44e89
taylorrsabin <taylor54sabin@gmail.com>
2022-04-07 22:40:39 -0600

    yep

--- Startup.cs
+++ Startup.cs
@@ -13,14 +13,20 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using IntexII.Models;
-using Microsoft.ML.OnnxRuntime;
 
 namespace IntexII
 {
     public class Startup
     {
+
         private async Task CreateRoles(IServiceProvider serviceProvider)
         {
+
+            var appUser = Environment.GetEnvironmentVariable("APP_USER");
+            var appEmail = Environment.GetEnvironmentVariable("APP_EMAIL");
+            var appPassword = Environment.GetEnvironmentVariable("APP_PASSWORD");
+            var appAdminEmail = Environment.GetEnvironmentVariable("APP_ADMINEMAIL");
+
             //initializing custom roles 
             var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
             var UserManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
@@ -40,13 +46,13 @@ namespace IntexII
             //Here you could create a super user who will maintain the web app
             var poweruser = new IdentityUser
             {
-                UserName = Configuration["AppSettings:UserName"],
-                Email = Configuration["AppSettings:UserEmail"],
+                UserName = appUser,
+                Email = appEmail,
                 EmailConfirmed = true,
             };
             //Ensure you have these values in your appsettings.json file
-            string userPWD = Configuration["AppSettings:UserPassword"];
-            var _user = await UserManager.FindByEmailAsync(Configuration["AppSettings:AdminUserEmail"]);
+            string userPWD = appPassword;
+            var _user = await UserManager.FindByEmailAsync(appAdminEmail);
 
             if (_user == null)
             {
@@ -72,26 +78,30 @@ namespace IntexII
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            var server = Environment.GetEnvironmentVariable("RDS_SERVER");
+            var port = Environment.GetEnvironmentVariable("RDS_PORT");
+            var user = Environment.GetEnvironmentVariable("RDS_USER");
+            var password = Environment.GetEnvironmentVariable("RDS_PASSWORD");
+            var database = Environment.GetEnvironmentVariable("RDS_DATABASE");
+            var gooId = Environment.GetEnvironmentVariable("GOO_CLIENTID");
+            var gooSecret = Environment.GetEnvironmentVariable("GOO_CLIENTSECRET");
+
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseMySql(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseMySql("server=" + server + ";port=" + port + ";database=" + database + ";uid=" + user + ";password=" + password));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
-            
 
             services.AddDbContext<RDSContext>(options =>
             {
-                options.UseMySql(Configuration["ConnectionStrings:RDSdatabase"]);
-
+                options.UseMySql("Data Source=" + server + ";Initial Catalog=" + database + ";User ID=" + user + ";Password=" + password + ";");
             });
 
 
-
-
-           services.Configure<IdentityOptions>(options =>
+            services.Configure<IdentityOptions>(options =>
             {
                 options.Password.RequireDigit = true;
                 options.Password.RequiredLength = 8;
@@ -108,16 +118,14 @@ namespace IntexII
                     //Configuration.GetSection("Authentication:Google");
                     //options.ClientId = googleAuthNSection["ClientId"];
                     //options.ClientSecret = googleAuthNSection["ClientSecret"];
-                    options.ClientId = Configuration["Authentication:Google:ClientId"];
-                    options.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+                    options.ClientId = gooId;
+                    options.ClientSecret = gooSecret;
 
                 });
 
             services.AddScoped<IRDSRepo, EFRDSRepo>();
             //services.AddScoped<ITeamsRepository, EFTeamsRepository>();
 
-            services.AddSingleton<InferenceSession>(
-                new InferenceSession("wwwroot/intex.onnx"));
 
         }
 
@@ -125,19 +133,59 @@ namespace IntexII
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
             }
+
             else
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.Use(async (context, next) =>
+                {
+                    context.Response.Headers.Add("Strict-Transport-Security", "max-age-31536000");
+                    context.Response.Headers.Add("X-Xss-Protection", "1");
+                    context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+                    context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+                    context.Response.Headers.Add("Referrer-Policy", "no-referrer");
+                    context.Response.Headers.Add("Feature-Policy",
+                    "vibrate 'self' ; " +
+                    "camera 'self' ; " +
+                    "microphone 'self' ; " +
+                    "speaker 'self'  ;  " +
+                    "geolocation 'self' ; " +
+                    "gyroscope 'self' ; " +
+                    "magnetometer 'self' ; " +
+                    "midi 'self' ; " +
+                    "sync-xhr 'self' ; " +
+                    "push 'self' ; " +
+                    "notifications 'self' ; " +
+                    "fullscreen '*' ; " +
+                    "payment 'self' ; ");
+
+                    context.Response.Headers.Add(
+                    "Content-Security-Policy-Report-Only",
+                    "default-src 'self'; " +
+                    "script-src-elem 'self'" +
+                    "style-src-elem 'self'; " +
+                    "img-src 'self'; http://www.w3.org/" +
+                    "font-src 'self'" +
+                    "media-src 'self'" +
+                    "frame-src 'self'" +
+                    "connect-src "
+
+                    );
+                    await next();
+                });
             }
 
             CreateRoles(serviceProvider).Wait();
+
+            //Security response headers
+            //
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
